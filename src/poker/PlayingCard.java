947c7461fb50732c286e874732b620f460139912
scart87 <shane.carty@hotmail.com>
2017-01-28 19:59:30 +0000

    defined playing card class

--- src/poker/PlayingCard.java
+++ src/poker/PlayingCard.java
@@ -2,5 +2,51 @@ package poker;
 
 public class PlayingCard {
 	
-
+	private String type;
+	private char suit;
+	private int faceValue;
+	private int gameValue;
+	
+	public static final char HEARTS = 'H';
+	public static final char DIAMONDS = 'D';
+	public static final char SPADES = 'S';
+	public static final char CLUBS = 'C';
+	
+	public PlayingCard(String type, char suit, int faceValue, int gameValue){
+		this.type = type;
+		this.suit = suit;
+		this.faceValue = faceValue;
+		this.gameValue = gameValue;
+	}
+	
+	public String toString(){
+		return type + suit;
+	}
+	
+	public char getSuit(){
+		return suit;
+	}
+	
+	public int getFaceValue(){
+		return faceValue;
+	}
+	
+	public int getGameValue(){
+		return gameValue;
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		PlayingCard card = new PlayingCard("A", 'H', 1, 14);
+		
+		PlayingCard[] deck = new PlayingCard[52];
+		
+		System.out.println("Card: " + card.toString());
+		
+	}
+	
+	
 }
