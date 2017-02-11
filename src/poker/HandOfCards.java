6e0353f73c27f50251d648776c6b572bcbd7615f
scart87 <shane.carty@hotmail.com>
2017-02-11 23:04:05 +0000

    constructor and getCards method to hand of cards class

--- src/poker/HandOfCards.java
+++ src/poker/HandOfCards.java
@@ -1,6 +1,20 @@
 package poker;
 
 public class HandOfCards {
+	
+	private static int CARDS_PER_HAND = 5;
+	
+	private PlayingCard[] cards = new PlayingCard[CARDS_PER_HAND]; 
+	
+	public PlayingCard[] getCards(){
+		return cards;
+	}
+	
+	public HandOfCards(DeckOfCards deckOfCards){
+		for(int i = 0; i < CARDS_PER_HAND; i++){
+			cards[i] = deckOfCards.dealNext();
+		}
+	}	
 
 	public static void main(String[] args) {
 
